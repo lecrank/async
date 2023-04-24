a2088c48a68ffcadb974cc7f3d6c7b6b2e381da9
saul <kia22u475@student.bmstu.ru>
2023-04-24 12:36:58 +0300

    8s solution, sort method change

--- signer/signer.go
+++ signer/signer.go
@@ -15,29 +15,28 @@ func ExecutePipeline(jobs ...job) {
 	var wg sync.WaitGroup
 
 	for _, CurrJob := range jobs {
-		// fmt.Println("START Job:", CurrJob)
 		wg.Add(1)
-		go func(job job, in, out chan interface{}) {
+		go func(job job, in, out chan interface{}, w *sync.WaitGroup) {
 			defer close(out)
 			defer wg.Done()
-			// fmt.Println("Executing")
 			job(in, out)
-		}(CurrJob, inCh, outCh)
+		}(CurrJob, inCh, outCh, &wg)
 		inCh = outCh
 		outCh = make(chan interface{})
 	}
+	wg.Wait()
 }
 
-type Item struct {
-	Id  int
-	Hsh string
-}
+// type Item struct {
+// 	Id  int
+// 	Hsh string
+// }
 
-type ById []Item
+// type ById []Item
 
-func (a ById) Len() int           { return len(a) }
-func (a ById) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ById) Less(i, j int) bool { return a[i].Id < a[j].Id }
+// func (a ById) Len() int           { return len(a) }
+// func (a ById) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+// func (a ById) Less(i, j int) bool { return a[i].Id < a[j].Id }
 
 func SingleHash(in, out chan interface{}) {
 	var wg sync.WaitGroup
@@ -45,6 +44,13 @@ func SingleHash(in, out chan interface{}) {
 
 	for num := range in {
 		fmt.Println("SingleHash got", num)
+		intNum := num.(int)
+		// if num != nil {
+		// 	fmt.Println("OK: num is", num)
+		// 	intNum = num.(int)
+		// } else {
+		// 	fmt.Println(">> num is", num)
+		// }
 
 		dataConn := make(chan string, 1)
 
@@ -57,15 +63,15 @@ func SingleHash(in, out chan interface{}) {
 			md5Data := DataSignerMd5(data)
 			m.Unlock()
 			connector <- DataSignerCrc32(md5Data)
-		}(in, dataConn, strconv.Itoa(num.(int)), &wg, &m)
+		}(in, dataConn, strconv.Itoa(intNum), &wg, &m)
 
-		go func(in chan interface{}, connector chan string, out chan interface{}, w *sync.WaitGroup, n interface{}) {
+		go func(in chan interface{}, connector chan string, out chan interface{}, w *sync.WaitGroup, n int) {
 			defer close(connector)
 			defer w.Done()
-			resultData := DataSignerCrc32(strconv.Itoa((<-in).(int))) + "~" + (<-connector)
-			fmt.Println(n.(int), "SingleHash result", resultData)
+			resultData := DataSignerCrc32(strconv.Itoa(n)) + "~" + (<-connector)
+			fmt.Println(n, "SingleHash result", resultData)
 			out <- resultData
-		}(in, dataConn, out, &wg, num)
+		}(in, dataConn, out, &wg, intNum)
 	}
 	wg.Wait()
 }
@@ -74,25 +80,29 @@ func MultiHash(in, out chan interface{}) {
 
 	for hash := range in {
 		fmt.Println("MultiHash got", hash)
-		var Items []Item
+		Items := make([]string, 6)
 		var wg sync.WaitGroup
+		var m sync.Mutex
 		var multiResult string
 
 		wg.Add(6)
 		for i := 0; i < 6; i++ {
-			go func(data string, i int, results []Item, wg *sync.WaitGroup) {
+			go func(data string, i int, results []string, wg *sync.WaitGroup, m *sync.Mutex) {
 				defer wg.Done()
 				hash := DataSignerCrc32(strconv.Itoa(i) + data)
 				fmt.Println("Multihash", i, hash)
-				results = append(results, Item{i, hash})
-			}(hash.(string), i, Items, &wg)
+				m.Lock()
+				results[i] = hash
+				m.Unlock()
+			}(hash.(string), i, Items, &wg, &m)
 		}
 		wg.Wait()
-		sort.Sort(ById(Items))
+		// sort.Sort(ById(Items))
 
-		for _, item := range Items {
-			multiResult += item.Hsh
-		}
+		// for _, item := range Items {
+		// 	multiResult += item.Hsh
+		// }
+		multiResult = strings.Join(Items, "")
 		fmt.Println("MultiHash sorted:", multiResult)
 		out <- multiResult
 	}
